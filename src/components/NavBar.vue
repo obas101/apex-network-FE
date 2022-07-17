dd3f8892783162f00682993e53c04300180c836e
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-07-17 02:49:58 +0100

    branch

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -40,6 +40,42 @@
           Get Started Free
         </a>
       </div>
+
+      <!-- Hamburger secgtion -->
+      <button class="md:hidden hamburger focus:outline-none block">
+        <span class="hamburger-top"></span>
+        <span class="hamburger-middle"></span>
+        <span class="hamburger-bottom"></span>
+      </button>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div class="md:">
+      <div
+        id="menu"
+        class="
+          absolute
+          flex flex-col
+          items-center
+          self-end
+          hidden
+          py-8
+          mt-10
+          space-y-6
+          font-bold
+          bg-white
+          sm:w-auto sm:w-auto sm:self-center
+          left-6
+          right-6
+          drop-shadow-md
+        "
+      >
+        <a class="hover:text-primaryColor font-medium" href="">Demos</a>
+        <a class="hover:text-primaryColor font-medium" href="">About</a>
+        <a class="hover:text-primaryColor font-medium" href="">Blog</a>
+        <a class="hover:text-primaryColor font-medium" href="">Pages</a>
+        <a class="hover:text-primaryColor font-medium" href="">Contact</a>
+      </div>
     </div>
   </nav>
 </template>
